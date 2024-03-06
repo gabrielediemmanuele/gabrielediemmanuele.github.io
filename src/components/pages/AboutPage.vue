fc3cc2eb7408e90463646681618420fe4f103a05
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-06 16:12:27 +0100

    feat: add component introPage and props!

--- src/components/pages/AboutPage.vue
+++ src/components/pages/AboutPage.vue
@@ -1,76 +1,33 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+import introPage from "../ui-element/introPage.vue";
 
 export default {
   data() {
     return {
-      title: "About",
+      about: {
+        title: ".about",
+        subtitle:
+          "Nato come artista, Graphic Designer per caso, Web-Dev per scelta.",
+        description: "",
+        introImagePath: "../../../public/images/aboutpage.png",
+      },
     };
   },
 
-  // components: {
-  //   MyComponent,
-  // },
+  components: {
+    introPage,
+  },
 };
 </script>
 
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-6">
-        <div class="text-cont">
-          <h1>Titolo</h1>
-          <h3>Sottotitolo</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
-            quas id officiis fugiat distinctio sequi sunt vero harum, nisi
-            adipisci dolorum fugit obcaecati recusandae. Tempore repudiandae
-            fugiat reiciendis unde asperiores.
-          </p>
-        </div>
-      </div>
-      <div class="col-6">
-        <div class="image-cont">
-          <img src="../../../public/images/aboutpage.png" alt="" />
-        </div>
-      </div>
-    </div>
-  </div>
+  <introPage
+    :title="about.title"
+    :subtitle="about.subtitle"
+    :description="about.description"
+    :imgPath="about.introImagePath"
+  >
+  </introPage>
 </template>
 
-<style lang="scss" scoped>
-.container-fluid {
-  font-family: "Rubik", sans-serif;
-  padding: 20px 80px 20px 80px;
-  color: rgb(3, 35, 54);
-  .row {
-    border-bottom: 1px solid rgb(172, 172, 172);
-    padding-bottom: 30px;
-
-    .text-cont {
-      font-size: 20px;
-      h1 {
-        font-size: 150px;
-      }
-      h3 {
-        font-size: 40px;
-        font-weight: 400;
-        margin: 20px 0 20px 0;
-      }
-    }
-
-    .image-cont {
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: flex-end;
-      img {
-        width: 400px;
-        box-shadow: 0px 0px 8px 1px black;
-        border-radius: 50%;
-      }
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/ui-element/introPage.vue
@@ -0,0 +1,78 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    title: String,
+    subtitle: String,
+    description: String,
+    imgPath: String,
+  },
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6">
+        <div class="text-cont">
+          <h1>{{ title }}</h1>
+          <h3>{{ subtitle }}</h3>
+          <p>
+            {{ description }}
+          </p>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="image-cont">
+          <img :src="imgPath" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container-fluid {
+  font-family: "Rubik", sans-serif;
+  padding: 20px 80px 20px 80px;
+  color: rgb(3, 35, 54);
+  .row {
+    border-bottom: 1px solid rgb(172, 172, 172);
+    padding-bottom: 30px;
+
+    .text-cont {
+      font-size: 20px;
+      h1 {
+        font-size: 120px;
+      }
+      h3 {
+        font-size: 40px;
+        font-weight: 400;
+        margin: 20px 0 20px 0;
+        font-style: italic;
+      }
+    }
+
+    .image-cont {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: flex-end;
+      img {
+        width: 400px;
+        box-shadow: 0px 0px 8px 1px black;
+        border-radius: 50%;
+      }
+    }
+  }
+}
+</style>
