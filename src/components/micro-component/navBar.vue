538573256900e769cd36aa3b9699ff703057ed37
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-06 11:21:34 +0100

    fix NavBar: Hamb-menu, style and behavior fixed. MediaQ+

--- src/components/micro-component/navBar.vue
+++ src/components/micro-component/navBar.vue
@@ -30,53 +30,85 @@ export default {
 </script>
 
 <template>
-  <div class="nav-bar">
-    <router-link
-      class="nav-link"
-      aria-current="page"
-      :to="{ name: 'homepage' }"
-    >
-      <img id="codewhips-logo" src="/images/logo-codewhisp.png" alt="" />
-    </router-link>
-    <div class="hamburgher-menu"></div>
-    <ul>
-      <li v-for="link in navLinks">
-        <router-link
-          class="link-name"
-          aria-current="page"
-          :to="`${link.linkPath}`"
-        >
-          {{ link.linkName }}
-        </router-link>
-      </li>
-    </ul>
-    <div class="social-container">
-      <a href="https://www.linkedin.com/in/gabriele-di-emmanuele/"
-        ><font-awesome-icon class="fa-brands" icon="fa-brands fa-linkedin"
-      /></a>
-      <a href="https://github.com/gabrielediemmanuele"
-        ><font-awesome-icon class="fa-brands" icon="fa-brands fa-github"
-      /></a>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <!-- Router Link Logo (Homepage)-->
+      <router-link
+        class="nav-link"
+        aria-current="page"
+        :to="{ name: 'homepage' }"
+      >
+        <img id="codewhips-logo" src="/images/logo-codewhisp.png" alt="" />
+      </router-link>
+      <!-- Hamburgher Menu-->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item" v-for="link in navLinks">
+            <router-link
+              class="link-name"
+              aria-current="page"
+              :to="`${link.linkPath}`"
+            >
+              {{ link.linkName }}
+            </router-link>
+          </li>
+          <li id="social-cont-ham-menu">
+            <a href="https://www.linkedin.com/in/gabriele-di-emmanuele/"
+              ><font-awesome-icon
+                class="fa-brands"
+                icon="fa-brands fa-linkedin"
+            /></a>
+            <a href="https://github.com/gabrielediemmanuele"
+              ><font-awesome-icon class="fa-brands" icon="fa-brands fa-github"
+            /></a>
+          </li>
+        </ul>
+      </div>
+      <div class="social-container">
+        <a href="https://www.linkedin.com/in/gabriele-di-emmanuele/"
+          ><font-awesome-icon class="fa-brands" icon="fa-brands fa-linkedin"
+        /></a>
+        <a href="https://github.com/gabrielediemmanuele"
+          ><font-awesome-icon class="fa-brands" icon="fa-brands fa-github"
+        /></a>
+      </div>
     </div>
-  </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
-.nav-bar {
+.navbar {
+  margin: 0 !important;
+  padding: 0;
+}
+
+.container-fluid {
   width: 100%;
-  height: 100px;
-  padding: 20px 70px 20px 70px;
+  padding: 0;
   background-color: rgb(3, 35, 54);
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
   box-sizing: border-box;
+
+  .navbar-toggler {
+    color: white;
+    background-color: white;
+    margin-right: 50px;
+  }
 }
 
 #codewhips-logo {
   width: 80px;
+  margin: 10px 15px 15px 50px;
   &:hover {
     opacity: 0.8;
     cursor: pointer;
@@ -86,7 +118,7 @@ export default {
 ul {
   list-style: none;
   padding: 0;
-  margin: 0;
+  margin: 0px auto;
 
   display: flex;
   justify-content: space-around;
@@ -109,6 +141,7 @@ ul {
 }
 
 .social-container {
+  margin-right: 50px;
   .fa-brands {
     font-size: 40px;
     color: white;
@@ -120,9 +153,40 @@ ul {
   }
 }
 
-@media screen and (max-width: 830px) {
-  ul {
+#social-cont-ham-menu {
+  display: none;
+  .fa-brands {
+    font-size: 40px;
+    color: white;
+    margin: 0px 10px 0px 10px;
+    &:hover {
+      color: orange;
+      cursor: pointer;
+    }
+  }
+}
+
+@media screen and (max-width: 992px) {
+  .social-container {
     display: none;
+  }
+
+  ul {
+    background-color: rgb(10, 61, 91);
+    box-shadow: inset 0px 8px 10px -4px rgba(0, 0, 0, 0.8);
+    padding-top: 10px;
+    li {
+      border-bottom: 1px solid rgb(3, 35, 54);
+      width: 100%;
+      text-align: center;
+      padding: 10px 0 10px 0;
+      .link-name {
+        font-size: 30px;
+      }
+    }
+  }
+  #social-cont-ham-menu {
+    display: block;
   }
 }
 </style>
