85b194b68a7954a2187727f1beecbb1c01dec578
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-07 16:55:38 +0100

    feat: about skills section end - add images for technologies container logos

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,6 @@ body {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: "Rubik", sans-serif;
 }
 </style>
--- src/components/about-page-components/aboutMeSkills.vue
+++ src/components/about-page-components/aboutMeSkills.vue
@@ -2,9 +2,32 @@
 export default {
   data() {
     return {
-      softwareSkills: [],
-      developerSkills: [],
+      designerSkillsTitle: "Designer 👨🏻‍🎨",
+      designerSkills: [
+        "Graphic Design",
+        "Ui/Ux Design",
+        "Design Systems",
+        "Graphic Solutions",
+        '"Simple is Better"',
+      ],
+      developerSkillsTitle: "Coder 👨🏻‍💻",
+      developerSkills: [
+        "Full-Stack developer",
+        "Front-End Addict",
+        "HTML / CSS",
+        "Java Script Souls Like",
+        "C0de Whisper",
+      ],
       technologiesImages: [
+        {
+          imagePath: "../../../public/images/marmore.jpg",
+        },
+        {
+          imagePath: "../../../public/images/marmore.jpg",
+        },
+        {
+          imagePath: "../../../public/images/marmore.jpg",
+        },
         {
           imagePath: "../../../public/images/marmore.jpg",
         },
@@ -31,20 +54,75 @@ export default {
 
 <template>
   <section class="skills-section">
+    <!--Skills Container-->
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-4 col-sm-12">
-          <div class="software-cont"></div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+          <div class="designer-skills common">
+            <h1>{{ designerSkillsTitle }}</h1>
+            <ul>
+              <li v-for="(designerSkill, index) in designerSkills" :key="index">
+                {{ designerSkill }}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="col-lg-4 col-sm-12">
-          <div class="developer-image-cont"></div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+          <div class="developer-image-cont">
+            <img src="../../../public/images/devAtWorking.jpg" alt="" />
+          </div>
         </div>
-        <div class="col-lg-4 col-sm-12">
-          <div class="developer-skil"></div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+          <div class="developer-skills common">
+            <h1>{{ developerSkillsTitle }}</h1>
+            <ul>
+              <li
+                v-for="(developerSkill, index) in developerSkills"
+                :key="index"
+              >
+                {{ developerSkill }}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   </section>
+  <!--Technologies Container-->
+  <section class="technologies"></section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container-fluid {
+  .row {
+    display: flex;
+    align-content: center;
+
+    .col-lg-4 {
+      .common {
+        padding: 60px 40px 40px 80px;
+        ul {
+          padding: 0;
+          margin: 0;
+          list-style: none;
+
+          li {
+            font-weight: 100;
+            margin: 20px 0px 20px 0px;
+          }
+        }
+      }
+
+      .developer-image-cont {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        img {
+          width: 250px;
+        }
+      }
+    }
+  }
+}
+</style>
