a7aed9ea85a99c09d84380af64054c9a105fa299
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-11 16:51:11 +0100

    feat: add to projectLayout the structure template

--- src/components/portfolio-page-components/projects-pages/frontEndBegin.vue
+++ src/components/portfolio-page-components/projects-pages/frontEndBegin.vue
@@ -1,21 +1,41 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+import introPage from "../../ui-element/introPage.vue";
+import projectLayout from "../../ui-element/projectLayout.vue";
 
 export default {
   data() {
     return {
-      title: "Hello world",
+      about: {
+        title: "ft-begin",
+        subtitle: "Progetti front-end realizzati all'inizio del corso.",
+        description:
+          "Tengo a mostrare i primissimi progetti realizzati in Boolean, anche per dare l'idea \
+          dei miglioramenti avuti nel tempo (e sicuramente c'è ancora tanto da imparare) \
+          ma vedere l'inizio del cammino, mostra anche quanta strada ho fatto!",
+        introImagePath:
+          "../../../../public/images/Front-End-Begin/portfolio-front-end-mix.png",
+      },
     };
   },
 
-  // components: {
-  //   MyComponent,
-  // },
+  components: {
+    introPage,
+    projectLayout,
+  },
 };
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <introPage
+    :title="about.title"
+    :subtitle="about.subtitle"
+    :description="about.description"
+    :imgPath="about.introImagePath"
+    :circular="false"
+    :rectangular="true"
+  ></introPage>
+
+  <projectLayout></projectLayout>
 </template>
 
 <style lang="scss" scoped></style>
--- src/components/ui-element/projectLayout.vue
+++ src/components/ui-element/projectLayout.vue
@@ -1,6 +1,4 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
 export default {
   data() {
     return {
@@ -15,7 +13,33 @@ export default {
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <!-- objectives -->
+  <section class="objectives-section">
+    <div class="container-fluid objectives-cont">
+      <h2 class="obj-title">Ciao</h2>
+      <p class="obj-paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        ipsum sit mollitia quia, eum, itaque cumque consectetur provident enim
+        inventore amet quo quam culpa? Iusto qui consequatur doloribus eius
+        alias!
+      </p>
+    </div>
+  </section>
+  <!-- Functions and video -->
+  <section class="functions-section">
+    <div class="row">
+      <div class="functionality-text col-lg-6 col-md-6 col-sm-12 col-12">
+        <h3 class="functions-title"></h3>
+        <p class="functions-paragraph"></p>
+      </div>
+      <div class="video-cont col-lg-6 col-md-6 col-sm-12 col-12"></div>
+    </div>
+  </section>
+  <!-- Images -->
+  <section class="images-section"></section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.objectives-cont {
+}
+</style>
