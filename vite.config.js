6ee8376023a45c4fe0e03ceba4996bb8263cd217
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-14 16:12:37 +0100

    try: vite.config.js settings 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/* export default defineConfig({
   plugins: [vue()],
-})
+}) */
+export default defineConfig({
+  base: "/gabrielediemmanuele.github.io/",
+  plugins: [],
+  resolve: {
+    alias: [],
+  },
+});
