ddc2183701a4949cb8db87d4719df400e9e6819b
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-18 15:54:06 +0100

    feat: add new Home page

--- src/components/ui-element/newHome.vue
+++ src/components/ui-element/newHome.vue
@@ -18,13 +18,15 @@ export default {
   <div class="container-fluid">
     <div class="text-cont">
       <h6>Ciao, mi chiamo</h6>
-      <h1 class="personal-name">Gabriele Di Emmanuele</h1>
-      <h1 class="competences">Coder and Web Designer</h1>
+      <h1 class="personal-name">Gabriele Di Emmanuele.</h1>
+      <h1 class="competences">Disegno codice per il web!</h1>
       <p>
-        Sono un !JR<span>Full-Stack Web Developer</span> con competenze in
+        Sono un !JR <span>Full-Stack Web Developer</span> con competenze in
         <span>Graphic - UI/UX Design</span>. <br />
         Al momento, sono concentrato nel migliorare le mie competenze sul
-        Front-End Development.
+        <span>Front-End Development</span>, <br />consolidando
+        <span>Java Script</span> e i miei prossimi obbiettivi saranno
+        <span>React</span> e <span>Angular</span>.
       </p>
       <button>
         <router-link
@@ -41,20 +43,20 @@ export default {
 <style lang="scss" scoped>
 .container-fluid {
   background-color: rgb(3, 35, 54);
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
   color: white;
-
   display: flex;
   justify-content: center;
 
   .text-cont {
-    margin: 50px auto;
+    margin: 20px auto 50px auto;
+    padding: 20px;
+    z-index: 1;
 
     h6,
     h1,
     p {
-      margin: 30px 0;
+      margin: 20px 0;
     }
 
     h6 {
@@ -63,8 +65,36 @@ export default {
       font-style: italic;
     }
 
+    h1 {
+      font-size: 50px;
+    }
+
     .competences {
       color: rgb(79, 149, 189);
+    }
+
+    p {
+      font-size: 15px;
+      line-height: 25px;
+      font-weight: 400;
+      color: rgb(141, 189, 218);
+      span {
+        color: orange;
+      }
+    }
+    button {
+      border: 1.5px solid orange;
+      padding: 8px 25px;
+      font-size: 15px;
+      color: orange;
+      transition: linear 5ms;
+      background-color: transparent;
+      border-radius: 12px;
+
+      &:hover {
+        color: white;
+        border: 1.5px solid rgb(255, 255, 255);
+      }
     }
   }
 }
