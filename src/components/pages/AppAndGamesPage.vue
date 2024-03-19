d2b4f27fb6cf1b547249bc202f9b23950f5ed04d
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-19 10:58:54 +0100

    feat: add to AppAndGames.vue: appsCont / gamesCont /.vue components

--- src/components/pages/AppAndGamesPage.vue
+++ src/components/pages/AppAndGamesPage.vue
@@ -1,21 +1,51 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+import introPage from "../ui-element/introPage.vue";
+import appsContainer from "../app-and-games-components/appsContainer.vue";
+import gamesContainer from "../app-and-games-components/gamesContainer.vue";
 
 export default {
   data() {
     return {
-      title: "Work in progress..!",
+      appsAndGames: {
+        title: ".apps & games",
+        subtitle: "Apps e giochi interattivi 🎮",
+        description:
+          "In questa sezione voglio mettere piccole app e giochi realizzati da me durante \
+          il mio percorso di studio, per rendere il sito più interattivo e piacevole! Quindi,\
+          non vi resta che provarli.. e ovviamente, 'sia lodato il sole'.",
+        introImagePath: "/images/dark-souls-bonfire.gif",
+        imageStyles: {
+          width: "50%",
+          transform: "scaleX(-1)",
+        },
+      },
     };
   },
 
-  // components: {
-  //   MyComponent,
-  // },
+  components: {
+    introPage,
+    appsContainer,
+    gamesContainer,
+  },
 };
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <!-- Intro-Page-Portfolio -->
+  <introPage
+    :title="appsAndGames.title"
+    :subtitle="appsAndGames.subtitle"
+    :description="appsAndGames.description"
+    :imgPath="appsAndGames.introImagePath"
+    :circular="false"
+    :rectangular="true"
+    :imageStyles="appsAndGames.imageStyles"
+  >
+  </introPage>
+  <!--  Projects cards  -->
+  <appsContainer> </appsContainer>
+  <!--   Graphic Design Section -->
+  <gamesContainer></gamesContainer>
 </template>
 
 <style lang="scss" scoped></style>
