a5f9fda8baf1ee4ab2e5dc1c68c2d41ff22d6426
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-19 15:33:08 +0100

    feat: add a lot of css to guessMyNumber

--- src/components/app-and-games-components/games/guessMyNumber.vue
+++ src/components/app-and-games-components/games/guessMyNumber.vue
@@ -63,44 +63,35 @@ export default {
 
 .main-container {
   color: #eee;
-  background-color: rgb(57, 68, 117);
+  background-color: #212121;
 }
 
 /* LAYOUT */
 header {
-  padding: 20px 30px;
+  padding: 15px 30px;
   height: 20vh;
   .container-fluid.header {
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: space-between;
-
-    .btn-cont {
-      .btn {
-        background-color: white;
-        padding: 10px 20px;
-      }
-      .btn {
-        margin: 10px;
-      }
-    }
   }
 }
 
 main {
   height: 80vh;
   .container-fluid.center {
-    padding-top: 20px;
     display: flex;
     flex-direction: column;
     align-items: center;
 
     .number {
-      border: 2px solid white;
-      border-radius: 15px;
-      padding: 50px;
+      //border: 2px solid white;
+      // border-radius: 15px;
+      padding: 25px 50px;
       margin-top: 35px;
+      font-size: 50px;
+      text-align: center;
     }
   }
   .container-fluid.left-right {
@@ -110,12 +101,144 @@ main {
       .right {
         display: flex;
         flex-direction: column;
+        margin: 30px auto;
+        //border: 1px dashed greenyellow;
+      }
+      /*left*/
+      .left {
+        align-items: center;
+        padding: 0px 130px;
+        .guess {
+          background: none;
+          border: 4px solid #eee;
+          display: block;
+          margin-bottom: 20px;
+          border-radius: 20px;
+          width: 220px;
+          padding: 10px;
+
+          font-family: inherit;
+          color: rgb(255, 255, 255);
+          font-size: 50px;
+          text-align: center;
+          overflow-y: hidden;
+        }
+      }
+      /* right  */
+      .right {
+        padding: 20px 130px;
+        p {
+          font-size: 20px;
+        }
+        .label-score,
+        .label-highscore {
+          margin: 15px 0px;
+        }
       }
     }
-    .left,
-    .right {
-      border: 1px dashed greenyellow;
-    }
   }
+}
+/* .btn {
+  background-color: white;
+  padding: 10px 20px;
+  margin: 10px;
+} */
+
+/*Button Custom*/
+.btn {
+  --border-radius: 15px;
+  --border-width: 4px;
+  appearance: none;
+  position: relative;
+  padding: 15px 30px;
+  margin: 10px;
+  border: 0;
+  background-color: #212121;
+  font-size: 18px;
+  font-weight: 500;
+  color: #fff;
+  z-index: 2;
+  &:hover {
+    color: orange;
+  }
+}
+
+.btn::after {
+  --m-i: linear-gradient(#000, #000);
+  --m-o: content-box, padding-box;
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  padding: var(--border-width);
+  border-radius: 15px;
+  background-image: conic-gradient(
+    #488cfb,
+    #299adb,
+    #bb74e1,
+    #970eff,
+    #e440bb,
+    #655adc,
+    #488cfb
+  );
+  -webkit-mask-image: var(--m-i), var(--m-i);
+  mask-image: var(--m-i), var(--m-i);
+  -webkit-mask-origin: var(--m-o);
+  mask-origin: var(--m-o);
+  -webkit-mask-clip: var(--m-o);
+  mask-composite: exclude;
+  -webkit-mask-composite: destination-out;
+  filter: hue-rotate(0);
+  animation: rotate-hue linear 2s infinite;
+  animation-play-state: running;
+}
+
+.btn:hover::after {
+  animation-play-state: paused;
+}
+
+@keyframes rotate-hue {
+  to {
+    filter: hue-rotate(1turn);
+  }
+}
+
+.btn,
+.btn::after {
+  box-sizing: border-box;
+}
+
+.btn:active {
+  --border-width: 5px;
+}
+
+//Bordo numero
+@keyframes rainbow {
+  0% {
+    border-color: #488cfb;
+  }
+  20% {
+    border-color: #299adb;
+  }
+  40% {
+    border-color: #bb74e1;
+  }
+  60% {
+    border-color: #970eff;
+  }
+  80% {
+    border-color: #e440bb;
+  }
+  100% {
+    border-color: #488cfb;
+  }
+}
+.number {
+  border-width: 3px;
+  border-style: solid;
+  border-radius: 10px;
+  animation: rainbow 5s infinite alternate;
 }
 </style>
