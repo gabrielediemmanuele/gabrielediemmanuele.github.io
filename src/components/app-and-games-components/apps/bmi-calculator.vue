07e4925d21c50b49892f292947ad7d55d87b0212
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2024-03-21 17:01:01 +0100

    feat: add bmi app layout

--- src/components/app-and-games-components/apps/bmi-calculator.vue
+++ src/components/app-and-games-components/apps/bmi-calculator.vue
@@ -15,7 +15,30 @@ export default {
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <!-- Inputs container -->
+  <div class="bmi-calculator" v-if="!showResult">
+    <input
+      v-model.number="weight"
+      type="number"
+      placeholder="Peso (Kg)"
+      :max="maxWeight"
+      required
+    />
+    <input
+      v-model.number="height"
+      type="number"
+      placeholder="Altezza (m, cm)"
+      :max="maxHeight"
+      required
+    />
+    <button @click="calculateBMI">Get BMI</button>
+  </div>
+  <!-- result container  -->
+  <div v-else>
+    <img :src="resultImage" alt="BMI Image" />
+    <p>{{ bmiMessage }}</p>
+    <button @click="reset">Back</button>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
